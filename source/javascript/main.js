09bef5887ac77976b90736020b16ddc18c3ce73d
TheDeveloperXYZ <contact@thedeveloper.xyz>
2016-11-27 00:56:37 +0200

    Update

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,3 +1,16 @@
-const func = () => `3 + 3 + 3 = ${3 + 3 + 3}`;
+import $ from 'jquery';
+import TiltFX from './tiltfx';
 
-console.log(func());
+$(document).ready(() => {
+  new TiltFX(document.querySelector('.intro__tilt__image'), {
+    'opacity' : 0.7,
+    'movement': {
+      'perspective' : 1000,
+      'translateX' : 70,
+      'translateY' : 10,
+      'translateZ' : 2,
+      'rotateX' : 3,
+      'rotateY' : 3
+    }
+  });
+});
